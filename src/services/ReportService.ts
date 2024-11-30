0248883bb2053a68da6b492d9c21a3433dba027d
João Graciotim <graciotim92@gmail.com>
2024-11-30 20:22:34 -0300

    adicionada nova util, controller, alterações na WebDriverFactory e ReportService

--- src/services/ReportService.ts
+++ src/services/ReportService.ts
@@ -1,37 +1,44 @@
-import { WebDriver, Builder, Browser, By, until, WebElement } from "selenium-webdriver";
+import { WebDriver } from "selenium-webdriver";
 import { data } from "../data/xpath";
-import { chromeOptions, downloadPath } from "../config/chromeConfig";
-import path from "path";
-import fs from "fs";
-import dotenv from 'dotenv';
 
 import { DateUtils } from "../utils/DateUtils";
-import { NameUtils } from "../utils/NameUtils";
 import { ElementInteractor } from "../interactors/ElementInteractor";
+import { ChromeDriverFactory } from "../factories/WebDriverFactory"; 
+import { FileUtils } from "../utils/FileUtils";
 
+import dotenv from 'dotenv';
 dotenv.config();
 
-export class ReportService {
+interface IReportService {
+    getReport(initialDate?: string, finalDate?: string): Promise<string | void> 
+}
+
+export class ReportService implements IReportService {
 
     dateUtils = new DateUtils(); 
-    nameUtils = new NameUtils();
+    fileUtils = new FileUtils();
     elementInteractor = new ElementInteractor();
+    chromeDriverFactory = new ChromeDriverFactory();
 
     private host: string = process.env.HOST!;
     private username: string = process.env.USER!;
     private password: string = process.env.PASSWORD!;
+    private downloadPath: string = process.env.DOWNLOAD_PATH!;
 
-    async initDriver(): Promise<WebDriver> {
-        return await new Builder().forBrowser(Browser.CHROME).setChromeOptions(chromeOptions).build();
-    }
 
-    
-
-    async getReport(initialDate: string = "01/01/2023", finalDate: string = this.dateUtils.getFormattedDate()): Promise<string | undefined> {
-        const driver: WebDriver = await this.initDriver();
+    async getReport(initialDate: string = "29/11/2024", finalDate: string = this.dateUtils.getFormattedDate()): Promise<string | void> {
+        const driver: WebDriver = this.chromeDriverFactory.createDriver(
+            {
+                headless: false,
+                gpuUsage: true,
+                sandbox: true,
+                devShmUsage: true,
+                downloadDirectory: this.fileUtils.formatPath(this.downloadPath)
+            }
+        );
 
         try {
-            console.log({ host: this.host, username: this.username, password: this.password })
+            console.log({ host: this.host, username: this.username, password: this.password, path: this.downloadPath })
 
             if (!this.host || !this.username || !this.password) {
                 throw new Error("Missing required environment variables.");
@@ -54,7 +61,7 @@ export class ReportService {
             await this.elementInteractor.findAndClick(driver, data.searchButton);
             await this.elementInteractor.findAndClick(driver, data.downloadButton);
 
-            const downloadedFilePath = await this.waitForDownloadToFinish(downloadPath);
+            const downloadedFilePath = await this.fileUtils.waitForDownloadToFinish(this.downloadPath);
 
             return downloadedFilePath;
 
@@ -63,37 +70,5 @@ export class ReportService {
         } finally {
             await driver.quit();
         }
-    }
-
-    async waitForDownloadToFinish(downloadPath: string, timeout = 1800000): Promise<string> {
-        const startTime = Date.now();
-        let downloaded = false;
-        let downloadedFilePath = '';
-
-        while (!downloaded) {
-            const files = fs.readdirSync(downloadPath);
-
-            // Verifica se o arquivo desejado foi baixado
-            const file = files.find((file: string) => file === 'Caixa_de_Entrada.xlsx');
-
-            if (file) {
-                // Pega o caminho completo do arquivo
-                downloadedFilePath = path.join(downloadPath, file);
-                downloaded = true;
-
-                // Renomeia o arquivo
-                const newFilePath = path.join(downloadPath, this.nameUtils.getNewName());
-                fs.renameSync(downloadedFilePath, newFilePath);
-                console.log(`Arquivo renomeado com sucesso: ${newFilePath}`);
-                downloadedFilePath = newFilePath;
-            } else if (Date.now() - startTime > timeout) {
-                throw new Error("O tempo de espera para o download expirou.");
-            } else {
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }
-        }
-
-        console.log(`Arquivo baixado com sucesso: ${downloadedFilePath}`);
-        return downloadedFilePath;
     }
 }
